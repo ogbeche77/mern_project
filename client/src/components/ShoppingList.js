e28e3a08e22024bf7b5184e6f8c5fe751cdb986c
ogbeche77 <ogbeche77@yahoo.com>
2020-03-04 18:01:05 +0100

    add/remove btn transition

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import  {v4 as uuidv4} from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            { id: uuidv4(), name:"Soap" },
+            { id: uuidv4(), name:"Sink" },
+            { id: uuidv4(), name:"Sponge" },
+            { id: uuidv4(), name:"Shampoo" }
+        ]
+    }
+    render() {
+        const {items} = this.state;
+        return(
+            <Container>
+            <Button color="dark"
+            style={{marginBottom: "2rem"}}
+            onClick={() => {
+                const name = prompt("Enter Item");
+                if(name) {
+                    this.setState(state => ({
+                        items:[...state.items, { id: uuidv4(), name}]
+                    }));
+                }
+            }}
+            >Add Item</Button>
+            <ListGroup>
+            <TransitionGroup className="shopping-list">
+            {items.map(({ id, name})=> (
+                <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                <Button className= "remove-btn" color="danger" size="sm"
+                onClick={()=>{
+                    this.setState(state => ({
+                        items: state.items.filter(item =>item.id !==id )
+                    }));
+
+                }}>&times;</Button>
+                {name}
+                </ListGroupItem>
+                </CSSTransition>
+
+            ))}
+            </TransitionGroup>
+            
+            </ListGroup>
+            </Container>
+            
+        );
+    }
+
+}
+
+export default ShoppingList;
