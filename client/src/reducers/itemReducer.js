354722f387e471bf6a6c242e20744c886bd16247
ogbeche77 <ogbeche77@yahoo.com>
2020-03-05 16:25:10 +0100

    del activated

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -15,7 +15,12 @@ export default function(state = initialState, action) {
         case GET_ITEMS:
             return {
                 ...state
-            }
+            };
+            case DELETE_ITEM:
+                return {
+                    ...state,
+                    items: state.items.filter(item =>item.id !==action.payload )
+                }
             default:
                 return state;
     }
