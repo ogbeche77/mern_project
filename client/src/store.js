1de788f3359c4abf140f0a0d78aa6c9bc6c84487
ogbeche77 <ogbeche77@yahoo.com>
2020-03-05 12:13:21 +0100

    , added

--- client/src/store.js
+++ client/src/store.js
@@ -6,7 +6,7 @@ const initialState ={};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState compose(
+const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
