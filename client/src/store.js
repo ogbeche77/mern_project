a9eebb7c15e680ca50d6db7a957c4759868b7313
ogbeche77 <ogbeche77@yahoo.com>
2020-03-05 13:47:44 +0100

    removed

--- client/src/store.js
+++ client/src/store.js
@@ -7,8 +7,8 @@ const initialState ={};
 const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    
 ));
 
 export default store;
