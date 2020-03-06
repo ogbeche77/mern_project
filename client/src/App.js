028de81dde1236c8601eedd2f0bf447f86ee4b83
ogbeche77 <ogbeche77@yahoo.com>
2020-03-06 10:16:08 +0100

    item modal imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
-
+import ItemModal from './components/ItemModal';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -14,6 +14,7 @@ class App extends Component {
       <Provider store ={store}>
       <div className="App">
       <AppNavbar />
+      <ItemModal />
       <ShoppingList />
       </div>
       </Provider>
