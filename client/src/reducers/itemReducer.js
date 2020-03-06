156f96c93bdd4e5a0cafe5d18450837d6990b996
ogbeche77 <ogbeche77@yahoo.com>
2020-03-06 10:41:46 +0100

    add item active

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 import  {v4 as uuidv4} from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEMS } from '../actions/types';
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types';
 
 const initialState = {
     items: [
@@ -21,6 +21,12 @@ export default function(state = initialState, action) {
                     ...state,
                     items: state.items.filter(item =>item.id !==action.payload )
                 }
+
+            case ADD_ITEMS:
+                return{
+                    ...state,
+                    items: [action.payload, ...state.items]
+                };
             default:
                 return state;
     }
