10c4597446df2b1a1045cead069f8209b54801c7
ogbeche77 <ogbeche77@yahoo.com>
2020-03-06 13:36:51 +0100

    items loading

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,14 +1,10 @@
 import  {v4 as uuidv4} from 'uuid';
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types';
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuidv4(), name:"Soap" },
-        { id: uuidv4(), name:"Sink" },
-        { id: uuidv4(), name:"Sponge" },
-        { id: uuidv4(), name:"ToothBrush" }
-    ]
-}
+    items: [],
+    loading:false
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
@@ -20,12 +16,16 @@ export default function(state = initialState, action) {
                 return {
                     ...state,
                     items: state.items.filter(item =>item.id !==action.payload )
-                }
-
+                };
             case ADD_ITEMS:
                 return{
                     ...state,
                     items: [action.payload, ...state.items]
+                };
+                case ITEMS_LOADING: 
+                return {
+                    ...state,
+                    loading: true
                 };
             default:
                 return state;
