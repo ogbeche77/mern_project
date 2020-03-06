13154d66d8cba1e83bd221114ea6f5ae252edfc3
ogbeche77 <ogbeche77@yahoo.com>
2020-03-06 10:42:03 +0100

    add item act

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -85,4 +85,8 @@ class ItemModal extends Component {
 
 }
 
-export default connect()(ItemModal);
+const mapStateToProps = state => ({
+    item:state.item
+});
+
+export default connect(mapStateToProps, {addItem})(ItemModal);
