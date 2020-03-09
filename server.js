20188cec22b98ed75a9e594228b4fba0b8b74949
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 17:32:25 +0100

    hero

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/items", items); //ensures all requests go to items variable
 // Serve static assets if in production
 if(process.env.NODE_ENV ==="production") {
     //Set static folder
-    app.use(express.static(path.join(__dirname,"client/build")));
+    app.use(express.static('client/build'));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
