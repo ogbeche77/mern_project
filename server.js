6bb7213cbf39b2326f0d770b6039037ac8dfa838
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 11:07:41 +0100

    errors fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require("express");
 const mongoose =require("mongoose"); // Mongoose to interact with MONGODB database
 const bodyParser =require("body-parser"); //To post request & get data from the body
 
-const items = require("./route/api/items");
+const items = require("./routes/api/items");  //We require the route in item.js
 
 const app = express(); // initialise express to var app
 
@@ -14,11 +14,13 @@ const db = require("./config/keys").mongoURI;
 
 //Connect to Mongo
 mongoose
-.connect(db)
+.connect(db, { useUnifiedTopology: true, useNewUrlParser: true})
 .then(()=> console.log("MongoDB is connected")) //shows in terminal to show we are connected
 .catch(err => console.log(err));
 
-app.use("./api/items", items);
+
+//Use Routes
+app.use("/api/items", items); //ensures all requests go to items variable
 
 const port = process.env.PORT || 5000  //because we may deloy to heroku (process.env.port)
 
