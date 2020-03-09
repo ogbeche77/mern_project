913bbd6a67667c172c2a6304cc9285c95f7257fb
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 13:33:16 +0100

    item reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import itemReducer from './itemReducer';
+import itemReducer from './itemReducer'; //we can have as many reducers as possible
 
 export default combineReducers({
     item: itemReducer
-})
+});
 
