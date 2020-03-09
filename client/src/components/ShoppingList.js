046a6a0620174a481a68914e036ac3277d520672
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 12:57:26 +0100

    error fixed

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -18,9 +18,7 @@ class ShoppingList extends Component {
         ]
     }
 
-    componentDidMount() {
-        this.props.getItems();
-    }
+    
 
     render() {
         const {items} = this.state;
@@ -36,43 +34,41 @@ class ShoppingList extends Component {
                     }));	
                 }	
             }}	
-            >Add Item</Button>	
+            >
+            Add Item
+            </Button>	
+
             <ListGroup>
             <TransitionGroup className="shopping-list">
             {items.map(({ id, name})=> (
                 <CSSTransition key={id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                
-                <Button className= "remove-btn" color="danger" size="sm"
-                onClick={()=> {
+                <Button
+                className ="remove-btn"
+                color="danger"
+                size="sm"
+                onClick={() => {
                     this.setState(state => ({
-                        items: state.items.filter(item =>item.id !==id)
+                        items: state.items.filter(item => item.id !==id)
                     }));
-                }}>&times;</Button>
+                }}
+                >
+                &times;
+                </Button> 
                 {name}
                 </ListGroupItem>
                 </CSSTransition>
+               
             ))}
             </TransitionGroup>
-
             </ListGroup>
             </Container>
 
         );
             }
         }
-        ShoppingList.propTypes = {	
-            getItems: PropTypes.func.isRequired,	    
-            item: PropTypes.object.isRequired	    
-        }
-    
-
-            
-const mapStateToProps = (state) => ({
-    item:state.item
-});
+        
 
 
-export default connect(mapStateToProps, 
-    { getItems })(ShoppingList);
+export default(ShoppingList);
 
