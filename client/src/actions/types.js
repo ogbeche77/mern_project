bf50d1c23bed4d2b2104ee5e920576def46d59cb
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 12:31:33 +0100

    pretiffied

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
-export const GET_ITEMS = 'GET_ITEMS';
+/*export const GET_ITEMS = 'GET_ITEMS';
 export const ADD_ITEMS = 'ADD_ITEMS';
 export const DELETE_ITEMS = 'DELETE_ITEMS';
 export const ITEMS_LOADING = 'ITEMS_LOADING';
+*/
