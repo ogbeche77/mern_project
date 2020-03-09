5bf14e9f317fae907675bea6d074539d887c233b
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 15:28:52 +0100

    item gotten from api

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -11,7 +11,9 @@ export default function(state = initialState, action) {
     switch(action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
                
             };
             case DELETE_ITEMS:
