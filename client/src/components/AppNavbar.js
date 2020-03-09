d018177806561d0a82ad57c66b934231bf3f297a
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 12:31:25 +0100

    toggle state changed

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; //create react component
 import {
     Collapse,
     Navbar,
@@ -12,35 +12,34 @@ import {
 
 class AppNavbar extends Component {
     state ={
-            isOpen: false
+            isOpen:false
         }
-toggle = () =>{
-    this.setState({
-        isOpen: !this.state.isOpen
+        toggle = () =>{ 
+            this.setState({
+            isOpen: !this.state.isOpen //change isOpen to whatever is not
     });
-
 }
-render() {
-    return (
-        <div>
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-        <Container>
-        <NavbarBrand href="/"> ShoppingList</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-        <NavItem>
-        <NavLink href="https//google.com">Google</NavLink>
-        </NavItem>
-        </Nav>
-        </Collapse>
-        </Container>
-        </Navbar>
-    
-    </div>
 
+//standard bootstrap attributes
+render() { 
+    return (
+    <div>
+    <Navbar color="dark" dark expand="sm" className="mb-5"> 
+    <Container>
+    <NavbarBrand href="/"> ShoppingList</NavbarBrand>
+    <NavbarToggler onClick={this.toggle} /> 
+    <Collapse isOpen={this.state.isOpen} navbar>
+    <Nav className="ml-auto" navbar>
+    <NavItem>
+    <NavLink href="https//google.com">Google</NavLink>
+    </NavItem>
+    </Nav>
+    </Collapse>
+    </Container>
+    </Navbar>
+
+</div>
     );
-    
 }
 }
 
