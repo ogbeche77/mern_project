dc18569668b3396063477369d9ca8654af474804
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 12:57:15 +0100

    error fixed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,7 @@
-/*import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
 
 export default combineReducers({
     item: itemReducer
 })
 
-*/
