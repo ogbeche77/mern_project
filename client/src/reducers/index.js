b2318826c7b38d5b3a50d45eb31f0d2a108ea111
ogbeche77 <ogbeche77@yahoo.com>
2020-03-09 12:30:49 +0100

    pretiffied

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
+/*import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
 
 export default combineReducers({
     item: itemReducer
 })
+
+*/
