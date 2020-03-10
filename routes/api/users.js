6c10ad292b71a0f71c31404b003a27eb6f15f255
ogbeche77 <ogbeche77@yahoo.com>
2020-03-10 15:59:00 +0100

    token added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require ("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 //User Model
 const Item = require("../../models/User"); //we bring in item models
@@ -36,13 +38,23 @@ User.findOne({ email })
             newUser.password = hash;
             newUser.save()
             .then(user=> {
-                res.json({
-                    user: {
-                        id: user.id,
-                        name:user.name,
-                        email: user.email
-                    }
-                });
+                jwt.sign(
+                    {id: user.id },
+                    config.get("jwtSecret"),
+                    { expiresIn: 3600},//token to last for an hour
+                     (err, token)=>{
+                         if(err) throw err;
+                         res.json({
+                             token,
+                            user: {
+                                id: user.id,
+                                name:user.name,
+                                email: user.email
+                            }
+                        });
+                     }
+                )
+              
             });
         })
     })
