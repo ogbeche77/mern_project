3791e30fd41d0dea17f1c9610f7822b8522f0b9b
ogbeche77 <ogbeche77@yahoo.com>
2020-03-10 13:16:02 +0100

    user validation & password hashed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require ("bcryptjs");
 
 //User Model
 const Item = require("../../models/User"); //we bring in item models
@@ -9,8 +10,43 @@ const Item = require("../../models/User"); //we bring in item models
 //Public access
 
 router.post("/", (req, res)=> {
-res.send("register")
-});
+const {name, email, password} = req.body;
 
+
+//Validation
+if(!name || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields"});
+}
+
+//Check if user exists
+User.findOne({ email })
+.then(user => {
+    if(user) return res.status(400).json({ msg: "User already exist"});
+     
+    const newUser = new User({ 
+        name,
+        email,
+        password
+    }); 
+    
+    // Salt created and hashed
+    bcrypt.genSalt(10,(err, salt)=> {
+        bcrypt.hash(newUser.password, salt, (err, hash) =>{
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+            .then(user=> {
+                res.json({
+                    user: {
+                        id: user.id,
+                        name:user.name,
+                        email: user.email
+                    }
+                });
+            });
+        })
+    })
+})
+});
    
 module.exports = router;
