c21877cdd0cc20f68fbe0d52554cd0570cb301e5
ogbeche77 <ogbeche77@yahoo.com>
2020-03-10 12:48:23 +0100

    user model setup

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required:true
+    },
+
+    email: {
+        type: String,
+        required:true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required:true
+    },
+
+    date: {
+        type: Date,
+        default:Date.now
+    }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
