76580466ab37f6b0301d0d56f8413597da658f02
ogbeche77 <ogbeche77@yahoo.com>
2020-03-11 13:02:22 +0100

    header and get token

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -15,4 +15,28 @@ import {
 export const loadUser = () => (dispatch, getState) => {
     // User loading
     dispatch({ type: USER_LOADING });
+
+//Get token from localstorage
+const token = getState().auth.token;
+// Headers
+const config ={
+    headers: {
+        "Content-type": "application/json"
+    }
+}
+
+//If token, add to headers
+if(token) {
+    config.headers["x-auth-token"] = token;
+}
+    axios.get("/api/auth/user", config)
+    .then(res => dispatch({
+        type: USER_LOADED,
+        payload: res.data
+    }))
+    .catch()err=> {
+        dispatch({
+            type: AUTH_ERROR
+        });
+    };
 }
