d2f12a63121ce19b5d65c077893c42b6810faa09
ogbeche77 <ogbeche77@yahoo.com>
2020-03-11 13:11:18 +0100

    loadUser

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,17 @@ import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
 import { Provider } from 'react-redux'; //in order to bring in store.js
 import store from './store';
+import { loadUser } from "./actions/authActions";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    store.dispatch(loadUser());
+  }
+
+
   render(){
     return (
       <Provider store ={store}>
