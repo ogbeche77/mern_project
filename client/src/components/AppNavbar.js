f3626424dcc1403b384b47967d3c1f32f041747f
ogbeche77 <ogbeche77@yahoo.com>
2020-03-12 16:40:10 +0100

    login props

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; //create react component
+import React, { Component, Fragment } from 'react'; //create react component
 import {
     Collapse,
     Navbar,
@@ -9,6 +9,8 @@ import {
     NavLink,
     Container 
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import RegisterModal from './auth/RegisterModal';
 import LoginModal from './auth/LoginModal';
 import Logout from './auth/Logout';
@@ -16,7 +18,12 @@ import Logout from './auth/Logout';
 class AppNavbar extends Component {
     state ={
             isOpen:false
+        };
+
+        static propTypes ={
+            auth: propTypes.object.isRequired
         }
+
         toggle = () =>{ 
             this.setState({
             isOpen: !this.state.isOpen //change isOpen to whatever is not
@@ -25,6 +32,32 @@ class AppNavbar extends Component {
 
 //standard bootstrap attributes
 render() { 
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks =(
+        <Fragment>
+        <NavItem>
+    <Logout/></NavItem>
+        </Fragment>
+    );
+
+    const guestLinks = (
+        <Fragment>
+        <NavItem>
+        <span className ="navbar-text mr-3">
+        <strong> {user ? `Welcome ${user.name}`  : " "}</strong>
+        </span>
+        </NavItem>
+        <NavItem>
+    <RegisterModal />
+    </NavItem>
+    <NavItem>
+    <LoginModal />
+    </NavItem>
+        
+        </Fragment>
+    )
+
     return (
     <div>
     <Navbar color="dark" dark expand="sm" className="mb-5"> 
@@ -33,14 +66,8 @@ render() {
     <NavbarToggler onClick={this.toggle} /> 
     <Collapse isOpen={this.state.isOpen} navbar>
     <Nav className="ml-auto" navbar>
-    <NavItem>
-    <RegisterModal />
-    </NavItem>
-    <NavItem>
-    <LoginModal />
-    </NavItem>
-    <NavItem>
-    <Logout/></NavItem>
+    { isAuthenticated ? authLinks : guestLinks}
+    
     </Nav>
     </Collapse>
     </Container>
@@ -50,5 +77,8 @@ render() {
     );
 }
 }
+const mapStateToProps = state => ({
+    auth: state.auth
+})
 
-export default AppNavbar;
+export default connect (mapStateToProps, null) (AppNavbar);
