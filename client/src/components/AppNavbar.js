8ff3dc71595e8d1b4f28870a291db08310dd507d
ogbeche77 <ogbeche77@yahoo.com>
2020-03-12 15:59:03 +0100

    login in navvbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -10,6 +10,7 @@ import {
     Container 
 } from 'reactstrap';
 import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
 import Logout from './auth/Logout';
 
 class AppNavbar extends Component {
@@ -34,6 +35,9 @@ render() {
     <Nav className="ml-auto" navbar>
     <NavItem>
     <RegisterModal />
+    </NavItem>
+    <NavItem>
+    <LoginModal />
     </NavItem>
     <NavItem>
     <Logout/></NavItem>
