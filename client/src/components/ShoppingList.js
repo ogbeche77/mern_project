8388d555b78a078d94c9fb9cb8d38e741bdc0d48
ogbeche77 <ogbeche77@yahoo.com>
2020-03-13 12:39:44 +0100

    delete authenticated

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -8,6 +8,13 @@ import PropTypes from 'prop-types'; //component properties should be in proptype
 
 
 class ShoppingList extends Component {
+    //items in redux are stored as props
+static propTypes  = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+}; 
+
     componentDidMount(){
         this.props.getItems();
     }
@@ -24,14 +31,15 @@ class ShoppingList extends Component {
             {items.map(({ _id, name})=> (
                 <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                <Button
-                className ="remove-btn"
-                color="danger"
-                size="sm"
-                onClick={this.onDeleteClick.bind(this, _id)}
-                >
-                &times;
-                </Button> 
+                { this.props. isAuthenticated ? (  <Button
+                    className ="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                    &times;
+                    </Button> ) : null }
+               
                 {name}
                 </ListGroupItem>
                 </CSSTransition>
@@ -44,14 +52,11 @@ class ShoppingList extends Component {
         );
             }
         }
-//items in redux are stored as props
-ShoppingList.propTypes  = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}       
+     
 
 const mapStateToProps =(state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { getItems, deleteItems })(ShoppingList);
