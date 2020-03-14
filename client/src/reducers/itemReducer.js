9be06656149e27b682709f4c0357b8dbf592b0f1
ogbeche77 <ogbeche77@yahoo.com>
2020-03-14 09:45:41 +0100

    comments

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,4 @@
-/*import  {v4 as uuidv4} from 'uuid';*/
+// The reducer is where item state goes
 import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
