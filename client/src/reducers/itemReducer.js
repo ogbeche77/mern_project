1343311c3133000b2b201bb6d61e2b4cbe3424d2
ogbeche77 <ogbeche77@yahoo.com>
2020-03-14 10:30:16 +0100

    comments

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -12,7 +12,7 @@ export default function(state = initialState, action) {
         case GET_ITEMS:
             return {
                 ...state,
-                items: action.payload,
+                items: action.payload,  
                 loading: false
                
             };
