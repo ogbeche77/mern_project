335b4a1725cb458b0d9f1cdb715703ce97b61514
ogbeche77 <ogbeche77@yahoo.com>
2020-03-15 11:47:16 +0100

    comment

--- client/src/components/auth/LoginModal.js
+++ client/src/components/auth/LoginModal.js
@@ -36,7 +36,7 @@ class LoginModal extends Component {
         const { error, isAuthenticated } = this.props;
         if(error !== prevprops.error) {
             //check for register error
-            if(error.id === "LOGIN_FAIL"){
+            if(error.id === "LOGIN_FAIL"){ //check for error of login fail
                 this.setState({ msg:error.msg.msg});
             }else{
                 this.setState({ msg: null});
