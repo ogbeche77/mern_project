0d71e4a237ac7a72d2247367971df192e358a30b
ogbeche77 <ogbeche77@yahoo.com>
2020-03-27 10:13:07 +0100

    color

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -33,7 +33,7 @@ static propTypes  = {
                 <ListGroupItem>
                 { this.props. isAuthenticated ? (  <Button
                     className ="remove-btn"
-                    color="warning"
+                    color="primary"
                     size="sm"
                     onClick={this.onDeleteClick.bind(this, _id)} //onclick, button passed id & calls method above
                     >
