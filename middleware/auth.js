efead724c4a1cfe0c0fe5627621106d3c76e64bd
ogbeche77 <ogbeche77@yahoo.com>
2020-04-02 10:32:00 +0200

    structure

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,22 @@
-//middleware for private route that are accesed by our webtoken
+//middleware for private route that are accessed by our webtoken
 const config = require("config");
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-    const token = req.header("x-auth-token");
+  const token = req.header("x-auth-token");
 
-
-//Check for token
-if(!token) return  res.status(401).json({ msg: "No token, authorization denied"});
-try{
- //Verify token
- const decoded = jwt.verify(token, config.get("jwtSecret"));
- //Add user from payload
- req.user= decoded;
- next(); //call the next middleware
-} catch(e){
-    res.status(400).json({ msg: "Token not valid"});
-
-}
-   
+  //Check for token
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  try {
+    //Verify token
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    //Add user from payload
+    req.user = decoded;
+    next(); //call the next middleware
+  } catch (e) {
+    res.status(400).json({ msg: "Token not valid" });
+  }
 }
 
 module.exports = auth;
