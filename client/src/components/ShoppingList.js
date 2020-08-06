be301e95a4f2fcc32e5c37b462582c02eb3df0fb
ogbeche77 <ogbeche77@yahoo.com>
2020-08-06 12:12:28 +0200

    Code made pretty

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 /*import  {v4 as uuidv4} from 'uuid';*/
 import { connect } from 'react-redux';
 import { getItems, deleteItems } from '../actions/itemActions';
@@ -9,52 +9,52 @@ import PropTypes from 'prop-types'; //component properties should be in proptype
 
 class ShoppingList extends Component {
     //items in redux are stored as props
-static propTypes  = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired,
-    isAuthenticated: PropTypes.bool
-}; 
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getItems();
     }
     onDeleteClick = (id) => {   // methods to delete item is called
         this.props.deleteItems(id);
     };
-    
+
     render() {
-        const {items} = this.props.item;
-        return(
-            <Container>	      
-            <ListGroup>
-            <TransitionGroup className="shopping-list">
-            {items.map(({ _id, name})=> ( // all id changed to _id bcos item is now coming from MongoDB
-                <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                { this.props. isAuthenticated ? (  <Button
-                    className ="remove-btn"
-                    color="primary"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)} //onclick, button passed id & calls method above
-                    >
-                    &times;
-                    </Button> ) : null }
-               
-                {name}
-                </ListGroupItem>
-                </CSSTransition>
-               
-            ))}
-            </TransitionGroup>
-            </ListGroup>
+        const { items } = this.props.item;
+        return (
+            <Container>
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ _id, name }) => ( // all id changed to _id bcos item is now coming from MongoDB
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    {this.props.isAuthenticated ? (<Button
+                                        className="remove-btn"
+                                        color="primary"
+                                        size="sm"
+                                        onClick={this.onDeleteClick.bind(this, _id)} //onclick, button passed id & calls method above
+                                    >
+                                        &times;
+                                    </Button>) : null}
+
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
 
         );
-            }
-        }
-     
+    }
+}
 
-const mapStateToProps =(state) => ({
+
+const mapStateToProps = (state) => ({
     item: state.item,
     isAuthenticated: state.auth.isAuthenticated
 });
